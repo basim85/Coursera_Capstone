0ad002ee1611422879ab43231692adf90b2b628f
basim85 <72648250+basim85@users.noreply.github.com>
2020-10-13 12:21:09 +0200

    Add files via upload

---
+++ cap.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notbook will be used for my Data Science Capstone Project
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
